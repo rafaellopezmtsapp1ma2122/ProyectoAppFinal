c6b756304abcbb0107e0426ea405e4a8ec98696a
Rafa <rafael_lopez_m-tsapp1ma2122@cev.com>
2023-02-01 10:48:38 +0100

    Register add

---
+++ Proyecto/registerViewController.swift
@@ -0,0 +1,21 @@
+//
+//  registerViewController.swift
+//  Proyecto
+//
+//  Created by Apps2M on 1/2/23.
+//
+
+import UIKit
+
+class registerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("hola")
+    }
+    
+
+ 
+
+}
